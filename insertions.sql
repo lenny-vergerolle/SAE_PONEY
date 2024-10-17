a28ffda44feb6f55fbe74f2969e2f98a08d64c19
Akhtar Naima <o22304200@or-iut-i201-f10.iut45.univ-orleans.fr>
2024-10-17 11:59:03 +0200

    modifications du format des dates

--- insertions.sql
+++ insertions.sql
@@ -40,26 +40,27 @@ INSERT INTO MONITEUR (idMon, nomMon, prenomMon, ddnMon, sexeMon, telMon, mailMon
 
 
 INSERT INTO ADHERENT (idAdh, nomAdh, prenomAdh, ddnAdh, sexeAdh, telAdh, mailAdh, poidsAdh, cotisation) VALUES
-(1, 'Durand', 'Pierre', '2000-01-01', 'Homme', '0701234567', 'pierre.durand@email.com', 70, TRUE),
-(2, 'Roux', 'Chloé', '1995-02-14', 'Femme', '0712345678', 'chloe.roux@email.com', 55, TRUE),
-(3, 'Petit', 'Lucas', '1998-03-23', 'Homme', '0723456789', 'lucas.petit@email.com', 80, FALSE),
-(4, 'Blanc', 'Emma', '1992-04-30', 'Femme', '0734567890', 'emma.blanc@email.com', 60, TRUE),
-(5, 'Lemoine', 'Noah', '1987-05-15', 'Homme', '0745678901', 'noah.lemoine@email.com', 75, FALSE),
-(6, 'Giraud', 'Juliette', '1990-06-10', 'Femme', '0756789012', 'juliette.giraud@email.com', 50, TRUE),
-(7, 'Charpentier', 'Victor', '1985-07-05', 'Homme', '0767890123', 'victor.charpentier@email.com', 85, TRUE),
-(8, 'Faure', 'Alice', '1993-08-12', 'Femme', '0778901234', 'alice.faure@email.com', 68, FALSE),
-(9, 'Jacques', 'Léo', '1988-09-22', 'Homme', '0789012345', 'leo.jacques@email.com', 90, TRUE),
-(10, 'Boyer', 'Zoé', '1991-10-15', 'Femme', '0790123456', 'zoe.boyer@email.com', 58, TRUE),
-(11, 'Simon', 'Thomas', '1994-11-03', 'Homme', '0712345679', 'thomas.simon@email.com', 65, TRUE),
-(12, 'Perrot', 'Inès', '1996-12-25', 'Femme', '0723456780', 'ines.perrot@email.com', 54, TRUE),
-(13, 'Lefevre', 'Jules', '1999-01-19', 'Homme', '0734567891', 'jules.lefevre@email.com', 72, FALSE),
-(14, 'Cohen', 'Clara', '1986-02-27', 'Femme', '0745678902', 'clara.cohen@email.com', 61, TRUE),
-(15, 'David', 'Gabriel', '1990-03-30', 'Homme', '0756789013', 'gabriel.david@email.com', 76, TRUE),
-(16, 'Joly', 'Sophie', '1992-04-14', 'Femme', '0767890124', 'sophie.joly@email.com', 59, FALSE),
-(17, 'Mathieu', 'Alex', '1995-05-21', 'Homme', '0778901235', 'alex.mathieu@email.com', 88, TRUE),
-(18, 'Fischer', 'Laura', '1994-06-02', 'Femme', '0789012346', 'laura.fischer@email.com', 66, TRUE),
-(19, 'Simon', 'Emma', '1983-07-07', 'Femme', '0790123457', 'emma.simon@email.com', 63, FALSE),
-(20, 'Lemoine', 'Léa', '1991-08-19', 'Femme', '0701234568', 'lea.lemoine@email.com', 57, TRUE);
+(1, 'Durand', 'Pierre', STR_TO_DATE('01/01/2000', '%d/%m/%Y'), 'H', '0701234567', 'pierre.durand@email.com', 70, TRUE),
+(2, 'Roux', 'Chloé', STR_TO_DATE('14/02/1995', '%d/%m/%Y'), 'F', '0712345678', 'chloe.roux@email.com', 55, TRUE),
+(3, 'Petit', 'Lucas', STR_TO_DATE('23/03/1998', '%d/%m/%Y'), 'H', '0723456789', 'lucas.petit@email.com', 80, FALSE),
+(4, 'Blanc', 'Emma', STR_TO_DATE('30/04/1992', '%d/%m/%Y'), 'F', '0734567890', 'emma.blanc@email.com', 60, TRUE),
+(5, 'Lemoine', 'Noah', STR_TO_DATE('15/05/1987', '%d/%m/%Y'), 'H', '0745678901', 'noah.lemoine@email.com', 75, FALSE),
+(6, 'Giraud', 'Juliette', STR_TO_DATE('10/06/1990', '%d/%m/%Y'), 'F', '0756789012', 'juliette.giraud@email.com', 50, TRUE),
+(7, 'Charpentier', 'Victor', STR_TO_DATE('05/07/1985', '%d/%m/%Y'), 'H', '0767890123', 'victor.charpentier@email.com', 85, TRUE),
+(8, 'Faure', 'Alice', STR_TO_DATE('12/08/1993', '%d/%m/%Y'), 'F', '0778901234', 'alice.faure@email.com', 68, FALSE),
+(9, 'Jacques', 'Léo', STR_TO_DATE('22/09/1988', '%d/%m/%Y'), 'H', '0789012345', 'leo.jacques@email.com', 90, TRUE),
+(10, 'Boyer', 'Zoé', STR_TO_DATE('15/10/1991', '%d/%m/%Y'), 'F', '0790123456', 'zoe.boyer@email.com', 58, TRUE),
+(11, 'Simon', 'Thomas', STR_TO_DATE('03/11/1994', '%d/%m/%Y'), 'H', '0712345679', 'thomas.simon@email.com', 65, TRUE),
+(12, 'Perrot', 'Inès', STR_TO_DATE('25/12/1996', '%d/%m/%Y'), 'F', '0723456780', 'ines.perrot@email.com', 54, TRUE),
+(13, 'Lefevre', 'Jules', STR_TO_DATE('19/01/1999', '%d/%m/%Y'), 'H', '0734567891', 'jules.lefevre@email.com', 72, FALSE),
+(14, 'Cohen', 'Clara', STR_TO_DATE('27/02/1986', '%d/%m/%Y'), 'F', '0745678902', 'clara.cohen@email.com', 61, TRUE),
+(15, 'David', 'Gabriel', STR_TO_DATE('30/03/1990', '%d/%m/%Y'), 'H', '0756789013', 'gabriel.david@email.com', 76, TRUE),
+(16, 'Joly', 'Sophie', STR_TO_DATE('14/04/1992', '%d/%m/%Y'), 'F', '0767890124', 'sophie.joly@email.com', 59, FALSE),
+(17, 'Mathieu', 'Alex', STR_TO_DATE('21/05/1995', '%d/%m/%Y'), 'H', '0778901235', 'alex.mathieu@email.com', 88, TRUE),
+(18, 'Fischer', 'Laura', STR_TO_DATE('02/06/1994', '%d/%m/%Y'), 'F', '0789012346', 'laura.fischer@email.com', 66, TRUE),
+(19, 'Simon', 'Emma', STR_TO_DATE('07/07/1983', '%d/%m/%Y'), 'F', '0790123457', 'emma.simon@email.com', 63, FALSE),
+(20, 'Lemoine', 'Léa', STR_TO_DATE('19/08/1991', '%d/%m/%Y'), 'F', '0701234568', 'lea.lemoine@email.com', 57, TRUE);
+
 
 INSERT INTO COURS (idCo, nomCo, colllectif, nbPersonne, idMon) VALUES
 (1, 'Initiation au poney', TRUE, 10, 1),
@@ -74,16 +75,16 @@ INSERT INTO COURS (idCo, nomCo, colllectif, nbPersonne, idMon) VALUES
 (10, 'Initiation aux soins', TRUE, 6, 10);
 
 INSERT INTO RESERVER (duree, datee, heure, idCo, idPo, idAdh) VALUES
-(1, '2024-10-01', '10:00:00', 1, 1, 1),
-(2, '2024-10-01', '14:00:00', 2, 2, 2),
-(1, '2024-10-02', '09:00:00', 3, 3, 3),
-(2, '2024-10-02', '15:00:00', 4, 4, 4),
-(1, '2024-10-03', '11:00:00', 5, 5, 5),
-(1, '2024-10-03', '13:00:00', 6, 6, 6),
-(2, '2024-10-04', '12:00:00', 7, 7, 7),
-(1, '2024-10-04', '14:30:00', 8, 8, 8),
-(2, '2024-10-05', '10:00:00', 9, 9, 9),
-(1, '2024-10-05', '16:00:00', 10, 10, 10);
+(1, STR_TO_DATE('01/10/2024', '%d/%m/%Y'), '10:00:00', 1, 1, 1),
+(2, STR_TO_DATE('01/10/2024', '%d/%m/%Y'), '14:00:00', 2, 2, 2),
+(1, STR_TO_DATE('02/10/2024', '%d/%m/%Y'), '09:00:00', 3, 3, 3),
+(2, STR_TO_DATE('02/10/2024', '%d/%m/%Y'), '15:00:00', 4, 4, 4),
+(1, STR_TO_DATE('03/10/2024', '%d/%m/%Y'), '11:00:00', 5, 5, 5),
+(1, STR_TO_DATE('03/10/2024', '%d/%m/%Y'), '13:00:00', 6, 6, 6),
+(2, STR_TO_DATE('04/10/2024', '%d/%m/%Y'), '12:00:00', 7, 7, 7),
+(1, STR_TO_DATE('04/10/2024', '%d/%m/%Y'), '14:30:00', 8, 8, 8),
+(2, STR_TO_DATE('05/10/2024', '%d/%m/%Y'), '10:00:00', 9, 9, 9),
+(1, STR_TO_DATE('05/10/2024', '%d/%m/%Y'), '16:00:00', 10, 10, 10);
 
 INSERT INTO ENCADRER (idCo, idMon) VALUES
 (1, 1),
