91ed40d7ebe0a305d43c33bb5d749ce72c392f29
lenny-vergerolle <vergerolle.lenny@gmail.com>
2024-12-05 12:37:57 +0100

    mise en place de bootstrap

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="fr"><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Lenny Vergerolle , Valentin Hun , Naima Akhtar">
+
+    <title>Grand Galop</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    
+    <p class="text-success">Test</p>
+
+
+</body>
+</html>
