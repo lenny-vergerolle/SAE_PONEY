c7d9f197a034f903136cadb30d46918760f8f6f1
Naima Akhtar <naimasanaakhtar@gmail.com>
2025-01-06 17:54:02 +0100

    modification views.py et navbar.html

--- src/views.py
+++ src/views.py
@@ -1,4 +1,9 @@
+from flask import render_template
 from .app import app, db
 import src.views as views
 import src.commands as commands
 # import src.models as models
+
+@app.route("/navbar")
+def navbar():
+    return render_template('navbar.html')
